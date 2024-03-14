d2c723aaa7aa5d999abf48c0a677c20086b49180
Carsten Kremser <github@carstenkremser.de>
2024-03-14 11:39:22 +0100

    Implement main-Method: Use Library and Book (Exercise Step 5)

--- src/main/java/de/neuefische/cgnjava241/ooppackage/Main.java
+++ src/main/java/de/neuefische/cgnjava241/ooppackage/Main.java
@@ -2,6 +2,13 @@ package de.neuefische.cgnjava241.ooppackage;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Book[] myBooks = new Book[]{
+                new Book("Programmieren mit der Maus","Philip Kiefer","978-3-8421-0975-9"),
+                new Book("Looking good in Print","Roger C. Parker","3-907020-57-X"),
+                new Book("Rebel without a crew","Robert Rodriguez","978-0-452-27187-6"),
+        };
+        Library myLibrary = new Library(myBooks);
+        System.out.println(myLibrary);
     }
 }
