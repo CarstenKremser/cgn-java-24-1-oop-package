7663948a907659d6b0633887b280c3279d06d148
Carsten Kremser <github@carstenkremser.de>
2024-03-14 11:26:32 +0100

    Create method toString (Exercise Step 4)

--- src/main/java/de/neuefische/cgnjava241/ooppackage/Library.java
+++ src/main/java/de/neuefische/cgnjava241/ooppackage/Library.java
@@ -1,6 +1,13 @@
 package de.neuefische.cgnjava241.ooppackage;
 
+import java.util.Arrays;
+
 public class Library {
     Book[] books;
+
+    @Override
+    public String toString() {
+        return "{Library books:" + Arrays.toString(books) + "}";
+    }
 
 }
